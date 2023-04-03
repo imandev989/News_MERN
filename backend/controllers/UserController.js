e03a2adc6c182613b89f816ac8ce5d3575eb5610
imandev <iman.dev989@gmail.com>
2023-04-03 19:35:27 +0330

    session09 - complete register user in backend

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 import Users from "../models/userModel.js";
+import bcrypt from "bcrypt";
 
 export const getAllUsers = async (req, res) => {
   try {
@@ -10,10 +11,21 @@ export const getAllUsers = async (req, res) => {
 };
 
 export const Register = async (req, res) => {
+  const { name, email, password, confPassword, isAdmin } = req.body;
+  console.log(name, email, password, confPassword, isAdmin);
+  if (password !== confPassword) {
+    return res.json("پسورد و تکرار آن با هم برابر نیست");
+  }
+  const salt = await bcrypt.genSalt();
+  const hashPassword = await bcrypt.hash(password, salt);
   try {
-    const { name, email, password, confPassword, isAdmin } = req.body;
-    console.log(name, email, password, confPassword, isAdmin);
-    res.json("register");
+    await Users.create({
+      name: name,
+      email: email,
+      password: hashPassword,
+      isAdmin: isAdmin,
+    });
+    res.json({ message: "ثبت نام موفقیت آمیز بود" });
   } catch (error) {
     console.log(error);
   }
