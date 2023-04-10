7ac847eab7efed174ab3cae0f6ab87494404fb72
imandev <iman.dev989@gmail.com>
2023-04-10 18:02:46 +0330

    complete verifyToken session 15

--- backend/middleware/VerifyToken.js
+++ backend/middleware/VerifyToken.js
@@ -1,5 +1,12 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-}
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null)
+    return res.status(401).json("شما ابتدا باید وارد حساب کاربری خود شوید");
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+    if (err) return res.json("توکن منقضی شده است");
+    next();
+  });
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { getAllUsers, Login, Register } from "../controllers/UserController.js";
+import { verifyToken } from '../middleware/VerifyToken.js';
 
 const router = express.Router();
 
-router.get("/api/users", getAllUsers);
+router.get("/api/users", verifyToken, getAllUsers);
 router.post("/api/users/register", Register);
 router.get("/api/users/login", Login);
 
