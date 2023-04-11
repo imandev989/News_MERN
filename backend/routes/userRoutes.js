581a6c615961f65f791dae6c4c73fde174586e59
imandev <iman.dev989@gmail.com>
2023-04-11 18:00:06 +0330

    create RefreshToken and complete Authorization - session 16

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { getAllUsers, Login, Register } from "../controllers/UserController.js";
-import { verifyToken } from '../middleware/VerifyToken.js';
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { refreshToken } from "../controllers/RefreshToken.js";
 
 const router = express.Router();
+router.get("/token", refreshToken);
 
 router.get("/api/users", verifyToken, getAllUsers);
 router.post("/api/users/register", Register);
 router.get("/api/users/login", Login);
-
-
 
 export default router;
