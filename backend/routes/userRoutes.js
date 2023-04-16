acbc1c9b9f284e7aee03f9c09a19aebb8210be6e
imandev <iman.dev989@gmail.com>
2023-04-16 21:35:42 +0330

    crete logout in backend controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllUsers, Login, Register } from "../controllers/UserController.js";
+import { getAllUsers, Login, Logout, Register } from "../controllers/UserController.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 
@@ -7,7 +7,8 @@ const router = express.Router();
 router.get("/token", refreshToken);
 
 router.get("/api/users", verifyToken, getAllUsers);
-router.post("/api/users/register", Register);
+router.post("/api/users/register", verifyToken, Register);
 router.get("/api/users/login", Login);
+router.delete("/api/users/logout", verifyToken, Logout);
 
 export default router;
